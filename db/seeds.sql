25c7ea207a20a74ba819f3a15a359d40fdbbba6d
hsuzach <hsuzach@gmail.com>
2022-03-22 21:24:51 -0700

    insert sample data to seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,29 +1,33 @@
-DROP DATABASE IF EXISTS company_db;
-CREATE DATABASE company_db;
-
 USE company_db;
-CREATE TABLE department(
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30) NOT NULL
-);
 
-CREATE TABLE role(
-  id INT NOT NULL PRIMARY KEY
-  title VARCHAR(30) NOT NULL,
-  salary DECIMAL NOT NULL,
-  department_id INT NOT NULL,
-  FOREIGN KEY (department_id)
-  REFERENCES department(id) ON DELETE SET NULL
-);
+INSERT INTO department
+    (name)
+VALUES
+    ('Sales'),
+    ('Engineering'),
+    ('Finance'),
+    ('Legal');
 
-CREATE TABLE employees(
-  id INT NOT NULL PRIMARY KEY,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  role_id INT NOT NULL,
-  manager_id INT,
-  FOREIGN KEY (role_id)
-  REFERENCES role(id) ON DELETE SET NULL,
-  FOREIGN KEY (manager_id)
-  REFERENCES employees(id)
-);
+INSERT INTO roles
+    (title, salary, department_id)
+VALUES
+    ('Sales Lead', 100000, 1),
+    ('Salesperson', 80000, 1),
+    ('Lead Engineer', 150000, 2),
+    ('Software Engineer', 120000, 2),
+    ('Account Manager', 160000, 3),
+    ('Accountant', 125000, 3),
+    ('Legal Team Lead', 250000, 4),
+    ('Lawyer', 190000, 4);
+
+INSERT INTO employees
+    (first_name, last_name, role_id, manager_id)
+VALUES
+    ('John', 'Doe', 1, NULL),
+    ('Mike', 'Chan', 2, 1),
+    ('Ashley', 'Rodriguez', 3, NULL),
+    ('Kevin', 'Tupik', 4, 3),
+    ('Kunal', 'Singh', 5, NULL),
+    ('Malia', 'Brown', 6, 5),
+    ('Sarah', 'Lourd', 7, NULL),
+    ('Tom', 'Allen', 8, 7);
